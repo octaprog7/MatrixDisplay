6cab7c6d0959041dad8a15bd4f0c735e5db7f0fa
octaprog7 <goctaprog@gmail.com>
2022-11-17 17:19:05 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from machine import Pin, SPI
+import mtrx_disp
+from sensor_pack.bus_service import SpiAdapter
+import utime
+
+
+if __name__ == '__main__':
+    # bus_spi = SPI(0, baudrate=1_000_000, polarity=0, phase=0, firstbit=SPI.MSB)
+    bus_spi = SPI(0)
+    chip_select = Pin(5, mode=Pin.OUT, value=True)
+    adapter = SpiAdapter(bus=bus_spi, data_mode=None)
+    display = mtrx_disp.Lmd7219(adapter, chip_select, 4)
+    # display.text('1234', 0, 0, 1)
+    # display.show()
+
+    br = 0
+    while True:
+        lt = utime.localtime()
+        str_t = f"{lt[4]:02}{lt[5]:02}"
+        display.text(str_t, 0, 0, 1)
+        display.show()
+        utime.sleep_ms(1000)
+        display.set_brightness(br)
+        print(f"text: {str_t}; brightness: {br}")
+        br += 1
+        if 16 == br:
+            br = 0
---
+++ mtrx_disp.py
@@ -0,0 +1,101 @@
+from sensor_pack.bus_service import SpiAdapter
+from sensor_pack.base_sensor import Device, check_value
+from machine import Pin
+from micropython import const
+import framebuf
+
+
+class Lmd7219(Device):
+    """MicroPython class for work with MAX7219 led matrix 8x8 display"""
+
+    cmd_nop = const(0)
+    cmd_digit0 = const(1)
+    cmd_decode_mode = const(9)
+    cmd_intensity = const(10)
+    cmd_scan_limit = const(11)
+    cmd_shutdown = const(12)
+    cmd_display_test = const(15)
+
+    def __init__(self, adapter: SpiAdapter, address: Pin, number: int):
+        """number - количество элементов дисплея 8х8 led точек. От 1 до 32."""
+        super().__init__(adapter, address, True)
+        self.number = check_value(number, range(1, 33), f"Invalid number: {number}")
+        self.buffer = bytearray(8 * number)
+        fb = framebuf.FrameBuffer(self.buffer, 8 * number, 8, framebuf.MONO_HLSB)
+        self.framebuf = fb
+        #
+        # Provide methods for accessing FrameBuffer graphics primitives. This is a workround
+        # because inheritance from a native class is currently unsupported.
+        # http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
+        # self.fill = fb.fill     # (col)
+        # self.pixel = fb.pixel   # (x, y[, c])
+        # self.hline = fb.hline   # (x, y, w, col)
+        # self.vline = fb.vline   # (x, y, h, col)
+        # self.line = fb.line     # (x1, y1, x2, y2, col)
+        # self.rect = fb.rect     # (x, y, w, h, col)
+        # self.fill_rect = fb.fill_rect  # (x, y, w, h, col)
+        self.text = fb.text  # (string, x, y, col=1)
+        # self.scroll = fb.scroll  # (dx, dy)
+        # self.blit = fb.blit  # (fbuf, x, y[, key])
+        #
+        self.msb_first = False
+        self.init()
+
+    def _setup_bus(self):
+        """Настройки для правильной передачи данных по шине.
+        Settings for correct bus communication"""
+        if self.msb_first:
+            # self.adapter.bus.firstbit = SPI.MSB
+            ...
+        else:
+            # self.adapter.bus.firstbit = SPI.LSB
+            ...
+        self.adapter.use_data_mode_pin = False
+
+    def _write(self, buf: bytes, enable_cs_ctrl: bool = True):
+        """Запись данных по шине адресату.
+        enable_cs_ctrl - включить управление выбором чипа
+        Writing data on the bus to the destination.
+        enable_cs_ctrl - enable chip selection control"""
+        # адаптер могут использовать несколько устройств на шине, поэтому,
+        # перед каждой записью в шину необходима настройка!
+        # the adapter can be used by several devices on the bus, therefore,
+        # before each write to the bus, configuration is necessary!
+        self._setup_bus()
+        # запись во все элементы отображения
+        if enable_cs_ctrl:
+            self.address.low()
+        for _ in range(self.number):
+            self.adapter.write(self.address, buf)
+        if enable_cs_ctrl:
+            self.address.high()
+
+    def init(self):
+        for command, data in (
+            (Lmd7219.cmd_shutdown, 0),
+            (Lmd7219.cmd_display_test, 0),
+            (Lmd7219.cmd_scan_limit, 7),
+            (Lmd7219.cmd_decode_mode, 0),
+            (Lmd7219.cmd_shutdown, 1),
+        ):
+            self._write(bytes((command, data)))
+
+    def set_brightness(self, value):
+        if not 0 <= value <= 15:
+            raise ValueError(f"Brightness out of range: {value}")
+        self._write(bytes((Lmd7219.cmd_intensity, value)))
+    
+    def show(self):
+        self._setup_bus()
+        num = self.number
+        for y in range(8):
+            self.address.low()
+            for m in range(num):
+                a = Lmd7219.cmd_digit0 + y
+                b = self.buffer[(y * num) + m]
+                # self.adapter.bus.write(bytearray((a, b)))
+                self.adapter.bus.write(bytearray((a, b)))
+            self.address.high()
+
+
+  
