fe03b9d67e2067cf2b40cda2f0db1162cfcd47fd
octaprog7 <goctaprog@gmail.com>
2022-11-18 23:49:40 +0500

    ...

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ import utime
 
 
 if __name__ == '__main__':
-    # bus_spi = SPI(0, baudrate=1_000_000, polarity=0, phase=0, firstbit=SPI.MSB)
-    bus_spi = SPI(0)
+    bus_spi = SPI(0, baudrate=1_000_000, polarity=0, phase=0, firstbit=SPI.MSB)
+    # bus_spi = SPI(0)
     chip_select = Pin(5, mode=Pin.OUT, value=True)
     adapter = SpiAdapter(bus=bus_spi, data_mode=None)
     display = mtrx_disp.Lmd7219(adapter, chip_select, 4)
-    # display.text('1234', 0, 0, 1)
-    # display.show()
     switch = False
 
     br = 0
     while True:
         lt = utime.localtime()
+        # min and sec
         str_t = f"{lt[4]:02}{lt[5]:02}"
         display.fill(0)   # clear frame buffer
         display.text(str_t, 0, 0, 1)
@@ -34,9 +33,9 @@ if __name__ == '__main__':
         # display.set_pixel(1, 1, 1)
         # display.set_pixel(10, 5, 1)
         display.show()
+        print(f"text: {str_t}; brightness: {br}")
         utime.sleep_ms(1000)
         display.set_brightness(br)
-        print(f"text: {str_t}; brightness: {br}")
         br += 1
         if 16 == br:
             br = 0
