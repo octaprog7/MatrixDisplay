acf36dfaf6a9fb6882aa87582011113482941007
octaprog7 <goctaprog@gmail.com>
2022-11-18 21:59:53 +0500

    ...

--- main.py
+++ main.py
@@ -12,14 +12,27 @@ if __name__ == '__main__':
     display = mtrx_disp.Lmd7219(adapter, chip_select, 4)
     # display.text('1234', 0, 0, 1)
     # display.show()
+    switch = False
 
     br = 0
     while True:
         lt = utime.localtime()
         str_t = f"{lt[4]:02}{lt[5]:02}"
         display.fill(0)   # clear frame buffer
-        # display.text(str_t, 0, 0, 1)
-        display.rect(0, 0, display.number<<3, 8, 1)
+        display.text(str_t, 0, 0, 1)
+
+        if switch:
+            a, b = 15, 16
+        else:
+            a, b = 16, 15
+
+        display.vert_line(a, 0, 3, 1)
+        display.vert_line(b, 5, 3, 1)
+
+        switch = not switch
+        # display.rect(0, 0, display.number << 3, 8, 1)
+        # display.set_pixel(1, 1, 1)
+        # display.set_pixel(10, 5, 1)
         display.show()
         utime.sleep_ms(1000)
         display.set_brightness(br)
